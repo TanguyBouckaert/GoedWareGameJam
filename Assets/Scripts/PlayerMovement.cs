d84e70acffb5672bead2b0f9a4a815902e7de1b6
TanguyBouckaert <tanguy.bouckaert@student.howest.be>
2024-10-31 11:54:12 +0100

    player animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,16 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] public float _playerSpeed = 0.5f;
+    public Image m_Image;
+
+    public Sprite[] MovingRightAnimation, MovingLeftAnimation;
+
+    [SerializeField] public float _playerSpeed = 0.5f, m_AnimationSpeed = .02f;
 
     [SerializeField] private InputAction _leftRight;
 
     public InputAction Up, Down;
 
-    private bool _ladder = false;
+    private bool _ladder = false, IsDone;
+
+    private int m_IndexSprite;
+
+    Coroutine m_CorotineAnim;
 
     private void OnEnable()
     {
@@ -28,19 +37,39 @@ public class PlayerMovement : MonoBehaviour
         Down.Disable();
     }
 
-
     // Update is called once per frame
     void Update()
     {
+        //if (_leftRight.ReadValue<float>() == 0)
+        //{
+        //    StopCoroutine(Func_PlayAnimUI(MovingRightAnimation));
+        //    StartCoroutine(Func_PlayAnimUI(MovingLeftAnimation));
+        //}
+
         //LeftRight movement
         if (_leftRight.ReadValue<float>() > 0)
-            transform.Translate(new Vector2(-_playerSpeed,0));
-        if(_leftRight.ReadValue<float>() < 0)
+        {
+           transform.Translate(new Vector2(-_playerSpeed, 0));
+
+            //StopCoroutine(Func_PlayAnimUI(MovingRightAnimation));
+            //StartCoroutine(Func_PlayAnimUI(MovingLeftAnimation));
+        }
+           
+            
+        if (_leftRight.ReadValue<float>() < 0)
+        {
             transform.Translate(new Vector2(_playerSpeed, 0));
+
+            //StopCoroutine(Func_PlayAnimUI(MovingLeftAnimation));
+            //StartCoroutine(Func_PlayAnimUI(MovingRightAnimation));
+        }
+            
 
         if (Up.ReadValue<float>() == 1 && _ladder)
             transform.Translate(new Vector2(0, _playerSpeed));
+
     }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -57,4 +86,17 @@ public class PlayerMovement : MonoBehaviour
         if (hitObj.layer == 8)
             _ladder = false;
     }
+
+    //IEnumerator Func_PlayAnimUI(Sprite[] m_SpriteArray)
+    //{
+    //    yield return new WaitForSeconds(m_AnimationSpeed);
+    //    if (m_IndexSprite >= m_SpriteArray.Length)
+    //    {
+    //        m_IndexSprite = 0;
+    //    }
+    //    m_Image.sprite = m_SpriteArray[m_IndexSprite];
+    //    m_IndexSprite += 1;
+    //    if (IsDone == false)
+    //        m_CorotineAnim = StartCoroutine(Func_PlayAnimUI(m_SpriteArray));
+    //}
 }

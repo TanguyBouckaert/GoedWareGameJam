c770993899c667551452b039414e2a9bef56b69a
TanguyBouckaert <tanguy.bouckaert@student.howest.be>
2024-11-01 13:34:37 +0100

    Removed Unussed animation logic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,21 +7,14 @@ using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Image m_Image;
 
-    public Sprite[] MovingRightAnimation, MovingLeftAnimation;
-
-    [SerializeField] public float _playerSpeed = 0.5f, m_AnimationSpeed = .02f;
+    [SerializeField] public float _playerSpeed = 0.5f;
 
     [SerializeField] private InputAction _leftRight;
 
     public InputAction Up, Down;
 
-    private bool _ladder = false, IsDone;
-
-    private int m_IndexSprite;
-
-    Coroutine m_CorotineAnim;
+    private bool _ladder = false;
 
     private void OnEnable()
     {
@@ -40,19 +33,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (_leftRight.ReadValue<float>() == 0)
-        //{
-        //    StopCoroutine(Func_PlayAnimUI(MovingRightAnimation));
-        //    StartCoroutine(Func_PlayAnimUI(MovingLeftAnimation));
-        //}
-
         //LeftRight movement
         if (_leftRight.ReadValue<float>() > 0)
         {
            transform.Translate(new Vector2(-_playerSpeed, 0));
 
-            //StopCoroutine(Func_PlayAnimUI(MovingRightAnimation));
-            //StartCoroutine(Func_PlayAnimUI(MovingLeftAnimation));
         }
            
             
@@ -60,8 +45,6 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.Translate(new Vector2(_playerSpeed, 0));
 
-            //StopCoroutine(Func_PlayAnimUI(MovingLeftAnimation));
-            //StartCoroutine(Func_PlayAnimUI(MovingRightAnimation));
         }
             
 
@@ -86,17 +69,4 @@ public class PlayerMovement : MonoBehaviour
         if (hitObj.layer == 8)
             _ladder = false;
     }
-
-    //IEnumerator Func_PlayAnimUI(Sprite[] m_SpriteArray)
-    //{
-    //    yield return new WaitForSeconds(m_AnimationSpeed);
-    //    if (m_IndexSprite >= m_SpriteArray.Length)
-    //    {
-    //        m_IndexSprite = 0;
-    //    }
-    //    m_Image.sprite = m_SpriteArray[m_IndexSprite];
-    //    m_IndexSprite += 1;
-    //    if (IsDone == false)
-    //        m_CorotineAnim = StartCoroutine(Func_PlayAnimUI(m_SpriteArray));
-    //}
 }
